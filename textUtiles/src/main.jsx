7c3234135d6e6e23d8e5c5155346a85b991d428e
karan-patel-17 <145982752+karan-patel-17@users.noreply.github.com>
2023-10-06 16:41:39 +0530

    1st Commit
    06/10/2023

---
+++ textUtiles/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App  from './App';
+
+const rootElement = document.getElementById('root');
+
+// Instead of ReactDOM.render, use createRoot
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
